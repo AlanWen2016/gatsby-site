d36e7c2063c1739fa33cc86594bdff3855ee81e6
alan_wen <alan_wen@kingdee.com>
2022-01-16 22:45:49 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
+      <Link to="/">Back to Home</Link>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
+}
+export default IndexPage
